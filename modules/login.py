c3f71b142672e75369cf272884b2b2b3ded9a5f4
赵怡然 <63996691+zhaodice@users.noreply.github.com>
2023-11-27 09:21:35 +0800

    多人物支持

--- modules/login.py
+++ modules/login.py
@@ -1,14 +1,19 @@
 import flask
 import os
 import uuid
-from shutil import copyfile
+import glob
+import shutil
 from modules.common import return_error, return_success
 
 user_login = flask.Blueprint('user_login', __name__)
 
+def copyfiles(src,dst):
+    file_names = glob.glob(src)
+    for file_name in file_names:
+        shutil.copy(file_name, dst)
+
+
 # 登录
-
-
 @user_login.route("/login", methods=['post'])
 def login():
     user_name = flask.request.values.get('user_name')
@@ -28,7 +33,7 @@ def login():
     os.makedirs(f"./chars/{user_name}/init_state", exist_ok=True)
     os.makedirs(f"./save/{user_name}", exist_ok=True)
     try:
-        copyfile('./chars/小雪.json', f'./chars/{user_name}/小雪.json')
+        copyfiles('./chars/*.json', f'./tmp/chars/{user_name}/')
     except:
         return return_error('登陆失败')
     data = {
@@ -38,8 +43,6 @@ def login():
     return return_success(data)
 
 # 游客登录
-
-
 @user_login.route("/login/tmp", methods=['post'])
 def login_tmp():
     user_name = 'tmp-' + str(uuid.uuid1()).replace('-', '')
@@ -48,7 +51,7 @@ def login_tmp():
     os.makedirs(f"./tmp/chars/{user_name}/init_state", exist_ok=True)
     os.makedirs(f"./tmp/save/{user_name}", exist_ok=True)
     try:
-        copyfile('./chars/小雪.json', f'./tmp/chars/{user_name}/小雪.json')
+         copyfiles('./chars/*.json', f'./tmp/chars/{user_name}/')
     except:
         return return_error('登陆失败')
     with open(save_path, 'w') as f:
